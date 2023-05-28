1da10bdf1ed2d51d7a41cc6ff4a2ba90838a6801
RainerTeixeira <raineroliveira94@hotmail.com>
2023-05-28 14:21:31 -0300

    patrimonio ....

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
+# Importações necessárias para o código
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+# Inicialização da aplicação Flask
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.secret_key = "Secret Key"
 
-app.config['SQLALCHEMY DATABASE_URL'] = 'mysql://root:''@localhost/crud'
-app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
+# Configurações do SQLAlchemy para a conexão com o banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = sqlalchemy(app)
+# Inicialização do SQLAlchemy
+db = SQLAlchemy(app)
 
+# Definição do modelo de dados para SQLAlchemy
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
 
-class Data(db.model):
-    id = db.column(db.innteger, primary_key=True)
-    name = db.column(db.String(100))
-    email = db.column(db.String(100))
-    phone = db.column(db.String(100))
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
 
+# Definição de rota para a página inicial da aplicação
 @app.route('/')
 def index():
     return render_template('index.html', bootstrap=bootstrap)
 
-
+# Execução da aplicação
 if __name__ == '__main__':
     app.run(debug=True)
