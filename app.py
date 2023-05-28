54821c9a5193d285d4478a5ee41fe79c9434cb8e
RainerTeixeira <raineroliveira94@hotmail.com>
2023-05-28 13:57:14 -0300

    patrimonio ....

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.secret_key = "Secret Key"
 
+app.config['SQLALCHEMY DATABASE_URL'] = 'mysql://root:''@localhost/crud'
+app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
+
+db = sqlalchemy(app)
+
+
+class Data(db.model):
+    id = db.column(db.innteger, primary_key=True)
+    name = db.column(db.String(100))
+    email = db.column(db.String(100))
+    phone = db.column(db.String(100))
 
 @app.route('/')
 def index():
