c9f7f2e83ad9143e3d348603ef9edd368bc3758a
RainerTeixeira <raineroliveira94@hotmail.com>
2023-05-28 17:31:44 -0300

    patrimonio,,,,,

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 # Importações necessárias para o código
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,7 +9,7 @@ bootstrap = Bootstrap(app)
 app.secret_key = "Secret Key"
 
 # Configurações do SQLAlchemy para a conexão com o banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/crud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/crud'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicialização do SQLAlchemy
@@ -25,12 +25,27 @@ class Data(db.Model):
     def __init__(self, name, email, phone):
         self.name = name
         self.email = email
-        self.phone = phone.
+        self.phone = phone
 
 # Definição de rota para a página inicial da aplicação
 @app.route('/')
-def index():
+def Index():
     return render_template('index.html', bootstrap=bootstrap)
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        return redirect (url_for('Index'))
+            
 
 # Execução da aplicação
 if __name__ == '__main__':
